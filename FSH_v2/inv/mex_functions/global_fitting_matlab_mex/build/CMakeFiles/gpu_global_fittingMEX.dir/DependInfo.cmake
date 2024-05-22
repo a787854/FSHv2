79a96e3cb3840f3d0611b524a1fb1b84255a76a9
yanghai yu <46798123+a787854@users.noreply.github.com>
2024-05-22 17:05:08 +0800

    Add files via upload

---
+++ FSH_v2/inv/mex_functions/global_fitting_matlab_mex/build/CMakeFiles/gpu_global_fittingMEX.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/yanghai/fsh_v2_cuda/matlab_cuda/global_fitting_matlab_mex/src/mex/global_fitting_gpu_mex.cpp" "/home/yanghai/fsh_v2_cuda/matlab_cuda/global_fitting_matlab_mex/build/CMakeFiles/gpu_global_fittingMEX.dir/src/mex/global_fitting_gpu_mex.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "USE_CUDA"
+  "USE_MEX"
+  "gpu_global_fittingMEX_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/yanghai/matlab/Polyspace/R2020b/extern/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
