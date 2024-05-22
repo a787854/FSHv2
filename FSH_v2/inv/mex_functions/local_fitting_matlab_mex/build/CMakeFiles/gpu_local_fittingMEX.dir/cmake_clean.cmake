6ad4d919586df27505db7849b4532393c95e9cf9
yanghai yu <46798123+a787854@users.noreply.github.com>
2024-05-22 17:14:11 +0800

    Add files via upload

---
+++ FSH_v2/inv/mex_functions/local_fitting_matlab_mex/build/CMakeFiles/gpu_local_fittingMEX.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cuda_compile_1.dir/src/lib/cuda/cuda_compile_1_generated_kernel.cu.o"
+  "CMakeFiles/gpu_local_fittingMEX.dir/src/mex/local_fitting_cuda_mex.cpp.o"
+  "lib/gpu_local_fittingMEX.mexa64"
+  "lib/gpu_local_fittingMEX.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gpu_local_fittingMEX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
